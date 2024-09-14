af026664374202ae8de25385ba1da43fa276ae90
Алексей windows <alexey_kadochnikov@mail.ru>
2024-09-14 13:16:49 +0300

    Add home route

--- app.py
+++ app.py
@@ -7,6 +7,11 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+@app.route('/')
+def home():
+    return "Welcome to my Flask API!"
+
+
 # Настройка базы данных SQLite
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
